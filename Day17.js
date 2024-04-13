1c4bc35e9081fa114727cde831f639ef14683256
VivEk SuRve <108250212+viveksurve08@users.noreply.github.com>
2024-04-13 18:54:48 +0530

    .

--- Day17.js
+++ Day17.js
@@ -1,6 +1,5 @@
 // 18: Write a function to convert a string to camelCase & snake_case.
 
-<<<<<<< HEAD
 // const toCamelCase = (str) => {
 //   str = str.trim().split(" ");
 
@@ -27,30 +26,3 @@ const toSnakeCase = (str) => {
 // console.log(toSnakeCase("helloWorldDeveloper"));
 
 console.log(toSnakeCase("hello World Developer")); // Output: hello_world_developer
-=======
-// const toCamelCase = (str) => {
-//   str = str.trim().split(" ");
-
-//   str = str.map((curElem, index) => {
-//     if (index === 0) {
-//       return curElem.toLowerCase();
-//     } else {
-//       return curElem.charAt(0).toUpperCase() + curElem.slice(1).toLowerCase();
-//     }
-//   });
-//   console.log(str);
-//   return str.join("");
-// };
-
-// // Example usage:
-// console.log(toCamelCase("hello world deVeloper")); //Output: helloWorldDeveloper
-
-// 2nd Example:
-
-const toSnakeCase = (str) => {
-    return str.replace(/\s+/g, "_").toLowerCase();
-  };
-  // console.log(toSnakeCase("helloWorldDeveloper"));
-  
-  console.log(toSnakeCase("hello World Developer")); // Output: hello_world_developer
-  
