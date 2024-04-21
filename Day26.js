b6ca8dd1eec22b4379399bfa31feb5f291177496
VivEk SuRve <108250212+viveksurve08@users.noreply.github.com>
2024-04-21 18:49:46 +0530

    Fibonacci series is a sequence of numbers

---
+++ Day26.js
@@ -0,0 +1,25 @@
+//* Write a function to find the nth Fibonacci number.
+
+//? The Fibonacci series is a sequence of numbers in which each number (Fibonacci number) is the sum of the two proceding ones. It starts with 0 and 1, and subsequent numbers are calculated by adding the last two numbers. So, the Fibonacci series looks like this: 0, 1, 1, 2, 3, 5, 8, 13, 21, ...
+
+// todo Fibonacci number is calculated using the following formula:
+// todo syntax: F(n)= F(n-1) + F(n-2), where, F(1)=F(2) = 1.
+
+const fibonacci = (num) => {
+    if (num <= 1) {
+      return num;
+    } else {
+      return fibonacci(num - 1) + fibonacci(num - 2);
+    }
+  };
+  
+  // Example usage:
+  console.log(fibonacci(0)); //Output: 0
+  console.log(fibonacci(1)); //Output: 1
+  console.log(fibonacci(2)); //Output: 1
+  console.log(fibonacci(3)); //Output: 2
+  console.log(fibonacci(4)); //Output: 3
+  console.log(fibonacci(5)); //Output: 5
+  console.log(fibonacci(6)); //Output: 8
+  console.log(fibonacci(7)); //Output: 13
+  
