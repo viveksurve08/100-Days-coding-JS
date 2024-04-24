8a7ed62371ca94ceca9c872cfe93d5395a12d7f7
VivEk SuRve <108250212+viveksurve08@users.noreply.github.com>
2024-04-24 13:48:22 +0530

    truncateString that two parameters

---
+++ Day28.js
@@ -0,0 +1,27 @@
+// * Write a function called truncateString that two parameters:
+
+//? str: A string that needs to be trancated.
+//? maxLength: An integer representing the maximum length of the string allowed.
+//? The function should truncate the input string str if its length exceeds the specified maxLength. If truncation occurs, the function should add '...' to the end of the truncated string.
+
+const truncateString = (str, count) => {
+    // using if else if
+    //   if (count <= 0) {
+    //     return str;
+    //   } else if (str.length > count) {
+    //     return str.slice(0, count).concat("...");
+    //   }
+  
+    //   using Ternary Operator one line code
+    return count <= 0 ? str : str.slice(0, count).concat("...");
+  };
+  
+  // Example usage:
+  console.log(truncateString("A-tisket a-tasket A green and yellow basket", 8));
+  //Output: "A-tisket..."
+  
+  //* Constraints:
+  //? The input string str will contain only alphanumeric characters and punctuation marks.
+  //? The maximum length maxLength will be a positive integer.
+  //? The output string length should not exceed maxlength + 3 characters due to the addition of '...'.
+  
