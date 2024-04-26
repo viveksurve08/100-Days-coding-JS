ec5c197a9981e8fd7325d99459f9740d2c471c97
VivEk SuRve <108250212+viveksurve08@users.noreply.github.com>
2024-04-26 16:27:53 +0530

    Recursive Number Range Generator

---
+++ Day30.js
@@ -0,0 +1,46 @@
+//* Coding Challenge: Recursive Number Range Generator
+
+//? Write a recursive function called numberRangeRecursive that generates an array containing consecutive numbers from a to b (inclusive).
+
+//* Input:
+//? a: An integer representing the starting number of the range.
+//? b: An integer representing the ending number of the range.
+//? arr: An optional parameter representing the array to store the numbers in the range. It defaults to an empty array.
+
+//* Output:
+//? An array containing consecutive numbers from a to b (inclusive).
+
+//* Constraints:
+//? a and b will be integers.
+//? a will be less than or equal to b.
+
+// 1st ways *best 
+// const numberRangeRecursive = (a, b, arr = []) => {
+//   if (a <= b) {
+//     arr.push(a);
+//     return numberRangeRecursive(a + 1, b, arr);
+//   }
+//   return arr;
+// };
+
+// 2nd way- this way are complex 
+
+const numberRangeRecursive = (a, b) => {
+    if (typeof a !== "number" || typeof b !== "number" || a > b) {
+      return [];
+    }
+    const generateRangeRecursive = (curr, end, arr) => {
+      if (curr > end) {
+        return arr;
+      }
+      arr.push(curr);
+      return generateRangeRecursive(curr + 1, end, arr);
+    };
+    let rangeArr = [];
+    return generateRangeRecursive(a, b, rangeArr);
+  };
+  
+  console.log(numberRangeRecursive(0.5)); //Output: []
+  console.log(numberRangeRecursive(3, 7)); //Output: [3, 4, 5, 6, 7]
+  console.log(numberRangeRecursive(-2, 2)); //Output: [-2, -1, 0, 1, 2]
+  
