790c7895965ee2a23ee4f4339bb3bbb08fd344fa
VivEk SuRve <108250212+viveksurve08@users.noreply.github.com>
2024-04-27 20:03:32 +0530

    Simple Password Validator

---
+++ Day31.js
@@ -0,0 +1,75 @@
+//* Coding Challenge: Simple Password Validator
+
+//* Write a function called simplePasswordValidator that takes a single parameter:
+
+//? password: A string representing the password to be validated.
+//? The function should validate the password based on the following criteria:
+
+//? The password must containat least one lowercase letter, one uppercase letter, and one digit.
+//? The length of the password must be at least 8 characters.
+//? The function should return true if the password meets all the criteria, otherwise it should return false.
+
+//* Input:
+//? password: A non-empty string representing the password to be valudated.
+
+//* Output:
+//? true if the password meets all the criteria.
+//? false otherwise.
+
+//* Constraints:
+//? The input string password will contain only alphanumeric characters and punctuation marks.
+
+// 1st way
+// const simplePasswordValidator = (password) => {
+//   let hasLowerCase = false;
+//   let hasUpperCase = false;
+//   let hasNumber = false;
+
+//   for (let char of password) {
+//     if (char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90) {
+//       hasUpperCase = true;
+//     } else if (char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122) {
+//       hasLowerCase = true;
+//     } else if (!isNaN(Number(char))) {
+//       hasNumber = true;
+//     }
+//   }
+//   if (!hasLowerCase || !hasUpperCase || !hasNumber || password.length < 8) {
+//     return false;
+//   }
+//   return true;
+// };
+
+// 2nd way
+// const simplePasswordValidator = (password) => {
+//   let hasLowerCase = /[a-z]/;
+//   let hasUpperCase = /[A-Z]/;
+//   let hasNumber = /[0-9]/;
+//   if (
+//     !hasLowerCase.test(password) ||
+//     !hasUpperCase.test(password) ||
+//     !hasNumber.test(password)
+//   ) {
+//     return false;
+//   }
+//   if (password.length < 8) {
+//     return false;
+//   }
+//   return true;
+// };
+
+// 3rd way - simple way
+const simplePasswordValidator = (password) => {
+    return (
+      /[a-z]/.test(password) && /[A-Z]/.test(password) && password.length >= 8
+    );
+  };
+  
+  console.log(simplePasswordValidator("afkdsfadsf")); //Output: false
+  console.log(simplePasswordValidator("afkdsfadsf1")); //Output: false
+  console.log(simplePasswordValidator("afkdsfadsf1A")); //Output: true
+  console.log(simplePasswordValidator("afkdsfadsf1Aa")); //Output: true
+  
+  // console.log(!isNaN("Vivek")); // Output: false
+  // console.log(isNaN("Vivek")); // Output: true
+  
